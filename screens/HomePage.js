79a403bd6a99e8ea636307b839b94d83ff05efe7
Prince-Sedem <sedemdev@gmail.com>
2024-08-25 23:33:52 +0100

    Add  Home screen

---
+++ screens/HomePage.js
@@ -0,0 +1,12 @@
+import { Text, View} from 'react-native';
+
+
+export default function HomePage() {
+    
+    return (
+        <View className="bg-red-300 flex-1 justify-center ">
+            <Text >Welcome to the home page</Text>
+        </View>
+
+    )
+}
