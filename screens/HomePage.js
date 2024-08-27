dffefe34853404ee71c1494a3127463140cf9118
Prince-Sedem <sedemdev@gmail.com>
2024-08-27 20:07:07 +0100

    Add header content

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,12 +1,39 @@
-import { Text, View} from 'react-native';
+import { Text, View, SafeAreaView, Image} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 
 
 export default function HomePage() {
     
     return (
-        <View className="bg-red-300 flex-1 justify-center ">
-            <Text >Welcome to the home page</Text>
-        </View>
+        <SafeAreaView className="flex-1 bg-white flex space-y-5" edges={["top"]}>
+            <StatusBar style="dark" />
+            <View className="flex-row justify-between items-center mx-5">
+                <View className="space-y-2">
+                    <Text style={{fontSize: hp(3.5)}}
+                    className="font-bold tracking-wider text-neutral-700"
+                    >
+                        READY TO
+                    </Text>
+                    <Text style={{fontSize: hp(3.5)}}
+                    className="font-bold tracking-wider text-rose-500"
+                    >
+                        WORKOUT
+                    </Text>
+
+                </View>
+                <View className="flex justify-center items-center space-y-2">
+                    <Image  source={require('../assets/images/avatar.png')} 
+                    style={{height: hp(10), width: hp(10)}} 
+                    className="rounded-full"
+                    />
+
+                </View>
+
+            </View>
+
+        </SafeAreaView>
 
     )
 }
